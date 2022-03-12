4382ae55c349e4af722084e78c239635661605ce
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-03-12 10:04:59 +0100

    Creando Cookies y Login

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 
-
-
-
-
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -20,7 +16,6 @@ app.get('/', (req, res) => {
 
 app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
-    console.log(req.body)
     const {email, password} = req.body
 
     const generatedUserId = uuidv4()
@@ -55,6 +50,31 @@ app.post('/signup', async (req, res) => {
         console.log(err)
     }
 
+})
+
+app.post('/login', async (req, res) =>{
+    const client = new MongoClient(uri)
+    const { email, password } = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({ email })
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({ token, userId: user.user_id, email })
+        }
+        res.status(400).send('Invalid Credentials')
+    }catch (err) {
+        console.log(err)
+    }
 })
 
 app.get('/users', async(req, res) => {
