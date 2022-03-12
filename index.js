f51c74462eb254b47ae60ca137be02c065f862ff
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-03-12 14:44:15 +0100

    Actualizando Usuarios

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 
+const uri = 'mongodb+srv://jesusbenagal:k0bayashi@cluster0.ocnap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -45,7 +47,7 @@ app.post('/signup', async (req, res) => {
             expiresIn: 60 * 24,
         })
 
-        res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+        res.status(201).json({ token, userId: generatedUserId })
     } catch (err) {
         console.log(err)
     }
@@ -69,7 +71,7 @@ app.post('/login', async (req, res) =>{
             const token = jwt.sign(user, email, {
                 expiresIn: 60 * 24
             })
-            res.status(201).json({ token, userId: user.user_id, email })
+            res.status(201).json({ token, userId: user.user_id })
         }
         res.status(400).send('Invalid Credentials')
     }catch (err) {
@@ -87,6 +89,37 @@ app.get('/users', async(req, res) => {
 
         const returnedUsers = await users.find().toArray()
         res.send(returnedUsers)
+    } finally {
+        await client.close()
+    }
+})
+
+app.put('/user', async (req, res) => {
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
+
+    try{
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = { user_id: formData.user_id }
+        const updateDocument = {
+            $set: {
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
+        }
+        const insertedUser = await users.updateOne(query, updateDocument)
+        res.send(insertedUser)
     } finally {
         await client.close()
     }
